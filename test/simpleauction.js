f3e72f76d0e509a582340d54c5da346864b56ba4
smartjerry007 <smartjerrydev@gmail.com>
2024-01-27 19:37:30 +1000

    sdfsd

--- test/simpleauction.js
+++ test/simpleauction.js
@@ -1,11 +1,10 @@
 const SimpleAuction = artifacts.require("SimpleAuction");
 
 contract("SimpleAuction", () => {
-    before(async () => {
-        const instance = await SimpleAuction.deployed();
-    });
+   
+        const instance = SimpleAuction.deployed();
 
-    it("", async () => {
-        
+    it("Bid", async () => {
+        await instance.bid(10, {from:"0xca61aedD0e26be8A2Fa3E6fD631C303c78561D38"})
     })
 })
